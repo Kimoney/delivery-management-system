d8a537f44b5c47b5adc03e2f0893412a326e3859
John <johntaykimani@gmail.com>
2024-03-20 16:56:29 +0300

    feat: riders model and db table created.

---
+++ lib/db/rider.py
@@ -0,0 +1,14 @@
+from base import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Rider(Base):
+    __tablename__ = 'riders'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, unique=True, nullable=False)
+    location = Column(String, unique=True, nullable=False)
+    # Define foreign key relationship with trucks table
+    truck_id = Column(Integer, ForeignKey('trucks.id'))
+    # Define relationship with Truck
+    truck = relationship('Truck', back_populates='riders')
