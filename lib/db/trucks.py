e160cbd59a09ad3e2eb6ebc3afc7503be4712c06
John <johntaykimani@gmail.com>
2024-03-20 16:49:52 +0300

    feat: trucks model and db table created.

--- lib/db/trucks.py
+++ lib/db/trucks.py
@@ -1,10 +1,13 @@
 from base import Base
-from sqlalchemy import create_engine, Column, Integer, String, UniqueConstraint
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 class Truck(Base):
     __tablename__ = 'trucks'
 
     id = Column(Integer, primary_key=True, nullable=False)
     reg_no = Column(String, unique=True, nullable=False)
-    capacity = Column(Integer, nullable=False)
+    truck_capacity = Column(Integer, nullable=False)
+    model = Column(String, nullable=False)
+    # Define relationship with Rider
+    riders = relationship("Rider", back_populates="truck") 
