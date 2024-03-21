4de74e1cf7775118e3774a381a52c9fa88f6a327
John <johntaykimani@gmail.com>
2024-03-21 12:01:34 +0300

    feat: add_truck  method added to DMS

--- lib/db/delivery_management_system.py
+++ lib/db/delivery_management_system.py
@@ -21,7 +21,14 @@ class DeliveryManagementSystem:
         order = Order(product=product, quantity=quantity, cost=cost, customer_name=customer_name, location=location)
         self.session.add(order)
         self.session.commit()
-        print(f"\033[92m Success!! Entry saved at {datetime.now()} \033[0m")
+        print(f"\033[92m Success!! Order created at {datetime.now()} \033[0m")
+
+    def add_truck(self, reg_no, truck_capacity, model):
+
+        truck = Truck(reg_no=reg_no, truck_capacity=truck_capacity, model=model)
+        self.session.add(truck)
+        self.session.commit()
+        print(f"\033[92m Success!! Truck saved at {datetime.now()} \033[0m")
 
 
 if __name__ == '__main__':
@@ -29,4 +36,6 @@ if __name__ == '__main__':
     trial = DeliveryManagementSystem("dms_test.db")
     print(f"\033[92m Success!! Database Created at {datetime.now()} \033[0m")
     trial.create_order("Laptop", 5, 2500, "Kimani", "Nairobi")
-    print(f"\033[92m Success!! Entry Created at {datetime.now()} \033[0m")    
+    print(f"\033[92m Success!! Order Created at {datetime.now()} \033[0m")
+    trial.add_truck("KDA 567K", 2500, "Mitsubishi FH")
+    print(f"\033[92m Success!! Truck Added at {datetime.now()} \033[0m")    
